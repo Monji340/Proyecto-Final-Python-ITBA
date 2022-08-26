674433bc5b8fa0cc1aaaf0cd7c206523ad6ada33
Federico Gomez Conde <fedeic8@gmail.com>
2022-08-26 19:31:02 -0300

    Se agregaron class para pedido de datos al usuario

--- Menu_principal.py
+++ Menu_principal.py
@@ -1,8 +1,65 @@
 import tkinter as tk
 
+######     Ventana para pedir datos al usuario - Actualización de datos    ######
+class ActualizacionDatos:
+    
+    def __init__(self):
+        self.ventana1 = tk.Tk()
+        self.ventana1.title('Actualización de Datos')
+
+        self.label1 = tk.Label(self.ventana1, text="Ingrese ticker a pedir:")
+        self.label1.grid(column=0, row=0)
+        self.dato1 = tk.StringVar()
+        self.entry1 = tk.Entry(self.ventana1, width=30, textvariable=self.dato1)
+        self.entry1.grid(column=1, row=0)
+
+        self.label2 = tk.Label(self.ventana1, text="Fecha inicio")
+        self.label2.grid(column=0, row=1)
+        self.dato2 = tk.StringVar()
+        self.entry2 = tk.Entry(self.ventana1, width=30, textvariable=self.dato2)         #Ver en que formato debe ingresar la fecha
+        self.entry2.grid(column=1, row=1)
+
+        self.label3 = tk.Label(self.ventana1, text="Fecha fin")
+        self.label3.grid(column=0, row=2)
+        self.dato3 = tk.StringVar()
+        self.entry3 = tk.Entry(self.ventana1, width=30, textvariable=self.dato3)         #Ver en que formato debe ingresar la fecha
+        self.entry3.grid(column=1, row=2)
+
+        self.boton1 = tk.Button(self.ventana1, text="Pedir datos", command=self.ingresar)
+        self.boton1.grid(column=1, row=3)
+
+        self.ventana1.mainloop()
+    
+    def ingresar(self):
+        self.ventana1.title("Pidiendo Datos...")     # Instanciar con programa de Actualizacion de datos
+        
+
+######     Ventana para pedir datos al usuario - Gráfico de Ticker    ######
+class GraficarTicker:
+
+    def __init__(self):
+        self.ventana1 = tk.Tk()
+        self.ventana1.title('Grafico de ticker')
+
+        self.label1 = tk.Label(self.ventana1, text="Ingrese ticker a graficar:")
+        self.label1.grid(column=0, row=0)
+        self.dato1 = tk.StringVar()
+        self.entry1 = tk.Entry(self.ventana1, width=30, textvariable=self.dato1)
+        self.entry1.grid(column=1, row=0)
+
+        self.boton1 = tk.Button(self.ventana1, text="Graficar", command=self.ingresar)
+        self.boton1.grid(column=1, row=3)
+
+        self.ventana1.mainloop()
+    
+    def ingresar(self):
+        self.ventana1.title("Graficando...")      # Instanciar con programa de graficar
+
+
+
 # definimos las acciones asociadas a las opciones de los menús
 def accion1():
-    texto.configure(text='Reemplazar/llamar programa de actualización de datos')
+    texto.configure(text='Pedir datos a la API')
 
 
 def acciona():
@@ -27,12 +84,12 @@ ventana.config(menu=barra_menus)
 menu = tk.Menu(barra_menus, tearoff=False)
 
 # añadimos opciones al menú indicando su nombre y acción asociado
-menu.add_command(label='Actualizacioón de datos', command=accion1)
+menu.add_command(label='Actualizacioón de datos', command=ActualizacionDatos)
 
 # creamos un submenú
 submenu = tk.Menu(menu, tearoff=False)
 submenu.add_command(label='Resumen', command=acciona)
-submenu.add_command(label='Gráfico de ticker', command=accionb)
+submenu.add_command(label='Gráfico de ticker', command=GraficarTicker)
 
 # añadimos un submenú al menú principal
 menu.add_cascade(label='Visualización de datos', menu=submenu)
