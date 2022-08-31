4fecc735d003b046589c991e312b2aeda5907022
Federico Gomez Conde <fedeic8@gmail.com>
2022-08-31 17:05:18 -0300

    Se modifica clase a una función y se retornan los datos ingresados

--- VentanaPrueba.py
+++ VentanaPrueba.py
@@ -2,42 +2,42 @@
 
 import tkinter as tk
 
-class ActualizacionDatos:
-    
-    def __init__(self):
-        self.ventana1 = tk.Tk()
+def actualizacionDatos():
+    ventana1 = tk.Tk()
+    ventana1.title('Actualización de Datos')
+    # Caja de texto con el ticker a pedir
+    label1 = tk.Label(ventana1, text="Ingrese ticker a pedir:")
+    label1.grid(column=0, row=0)
+    dato1 = tk.StringVar()
+    entry1 = tk.Entry(ventana1, width=30, textvariable=dato1)
+    entry1.grid(column=1, row=0)
+    # Caja de texto fecha de inicio a pedir
+    label2 = tk.Label(ventana1, text="Fecha inicio")
+    label2.grid(column=0, row=1)
+    dato2 = tk.StringVar()
+    entry2 = tk.Entry(ventana1, width=30, textvariable=dato2)         #Ver en que formato debe ingresar la fecha
+    entry2.grid(column=1, row=1)
+    # Caja de texto fecha fin a pedir
+    label3 = tk.Label(ventana1, text="Fecha fin")
+    label3.grid(column=0, row=2)
+    dato3 = tk.StringVar()
+    entry3 = tk.Entry(ventana1, width=30, textvariable=dato3)         #Ver en que formato debe ingresar la fecha
+    entry3.grid(column=1, row=2)
 
-        self.label1 = tk.Label(self.ventana1, text="Ingrese ticker a pedir:")
-        self.label1.grid(column=0, row=0)
-        self.dato1 = tk.StringVar()
-        self.entry1 = tk.Entry(self.ventana1, width=30, textvariable=self.dato1)
-        self.entry1.grid(column=1, row=0)
+    # Función para ingresar los datos
+    def boton():
+        def cerrarVentana():
+            ventana1.destroy()                #Funcion para cerrar la ventana
+        ventana1.title("Pidiendo Datos...")
+        ventana1.after(3000, cerrarVentana)          #Cerramos la ventana después de 3 segundos
 
-        self.label2 = tk.Label(self.ventana1, text="Fecha inicio")
-        self.label2.grid(column=0, row=1)
-        self.dato2 = tk.StringVar()
-        self.entry2 = tk.Entry(self.ventana1, width=30, textvariable=self.dato2)         #Ver en que formato debe ingresar la fecha
-        self.entry2.grid(column=1, row=1)
+    # Botón para envío de los datos
+    boton1 = tk.Button(ventana1, text="Pedir datos", command=boton)
+    boton1.grid(column=1, row=3)
 
-        self.label3 = tk.Label(self.ventana1, text="Fecha fin")
-        self.label3.grid(column=0, row=2)
-        self.dato3 = tk.StringVar()
-        self.entry3 = tk.Entry(self.ventana1, width=30, textvariable=self.dato3)         #Ver en que formato debe ingresar la fecha
-        self.entry3.grid(column=1, row=2)
+    ventana1.mainloop()
+    lista = [dato1.get(), dato2.get(), dato3.get()]
+    return lista
 
-        self.boton1 = tk.Button(self.ventana1, text="Pedir datos", command=self.ingresar)
-        self.boton1.grid(column=1, row=3)
-
-        self.ventana1.mainloop()
-    
-    def cerrarVentana(self):
-        self.ventana1.destroy()                #Funcion para cerrar la ventana
-
-    def ingresar(self):
-        self.ventana1.title("Pidiendo Datos...")
-        self.ventana1.after(3000, self.cerrarVentana)          #Cerramos la ventana después de 3 segundos
-
-    
-
-
-aplicacion1=ActualizacionDatos()
+datos = actualizacionDatos()
+print(datos)
